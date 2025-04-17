a223eb8156fe12ccd3749639b485d6db74aa1729
Berkan Eti <berkaneti34@gmail.com>
2025-04-17 19:41:09 +0300

    add refresh/access token configurations

--- backend/src/main/java/com/softeng/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/softeng/backend/config/SecurityConfig.java
@@ -1,20 +1,30 @@
 package com.softeng.backend.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        System.out.println("Configuring Security...");
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
+        Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+        logger.info("Configuring security filter chain");
         http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/api/test").permitAll()
+                        .anyRequest().authenticated()
+
                 )
-                .csrf(csrf -> csrf.disable());
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
