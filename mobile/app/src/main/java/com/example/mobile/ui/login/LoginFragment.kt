7cf6025a7dfe99c87baa9539a8a0cbe699240c9c
dogukanbas <bdgknn@gmail.com>
2025-04-19 22:24:25 +0300

    add generic uiState to handle api responses

--- mobile/app/src/main/java/com/example/mobile/ui/login/LoginFragment.kt
+++ mobile/app/src/main/java/com/example/mobile/ui/login/LoginFragment.kt
@@ -1,5 +1,5 @@
 package com.example.mobile.ui.login
-
+import com.example.mobile.utils.MenuItem
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,7 +15,11 @@ import com.example.mobile.MainActivity
 import com.example.mobile.R
 import com.example.mobile.model.User.User
 import com.example.mobile.model.User.UserType
+import com.example.mobile.ui.admin.AddUserFragment
+import com.example.mobile.utils.UiState
 import com.google.android.material.textfield.TextInputEditText
+import com.token.uicomponents.ListMenuFragment.IListMenuItem
+import com.token.uicomponents.ListMenuFragment.ListMenuFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,12 +30,24 @@ class LoginFragment : Fragment() {
     private lateinit var loginButton: Button
 
 
+    private fun getLogo(): Int {
+        return R.drawable.ic_launcher_foreground
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         emailInput = view.findViewById(R.id.emailInput)
         passwordInput = view.findViewById(R.id.passwordInput)
         loginButton = view.findViewById(R.id.loginButton)
@@ -51,13 +67,28 @@ class LoginFragment : Fragment() {
         lifecycleScope.launchWhenStarted {
             viewModel.loginState.collect { state ->
                 when (state) {
-                    is LoginViewModel.LoginState.Idle -> {}
-                    is LoginViewModel.LoginState.Loading -> {}
-                    is LoginViewModel.LoginState.Success -> {
+                    is UiState.Idle -> {}
+                    is UiState.Loading -> {}
+                    is UiState.Success -> {
                         Log.i("LoginFragment", "Login successful for user: ${User.personalNo}")
 
                         when (User.userType) {
                             UserType.ADMIN -> {
+                                val menuItems = mutableListOf<IListMenuItem>()
+                                //admin has two functionalities, add user, add department
+                                menuItems.add(MenuItem(
+                                    "Add User",
+                                    { (activity as MainActivity).replaceFragment(AddUserFragment()) }
+                                ))
+                                menuItems.add(MenuItem(
+                                    "Add Department",
+                                    { (activity as MainActivity).replaceFragment(AddUserFragment()) }
+                                ))
+                                val menuFragment = ListMenuFragment.newInstance(menuItems,"Admin Menu",true,getLogo())
+                                (activity as MainActivity).replaceFragment(menuFragment as Fragment)
+
+
+                                //add fragment AdminHome
 
                             }
                             UserType.MANAGER -> {
@@ -75,13 +106,13 @@ class LoginFragment : Fragment() {
                             }
                         }
                     }
-                    is LoginViewModel.LoginState.Error -> {
+                    is UiState.Error -> {
                         showError(state.message)
                     }
                 }
             }
         }
-        return view
+
     }
 
     private fun showError(message: String) {
