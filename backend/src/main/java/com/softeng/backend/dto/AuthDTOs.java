cad0233047f024fa9743fd725029c5127f8094e2
Berkan Eti <berkaneti34@gmail.com>
2025-04-20 16:25:40 +0300

    configure user & auth flow

--- backend/src/main/java/com/softeng/backend/dto/AuthDTOs.java
+++ backend/src/main/java/com/softeng/backend/dto/AuthDTOs.java
@@ -1,24 +1,7 @@
 package com.softeng.backend.dto;
-
 import com.softeng.backend.model.User;
 
 public class AuthDTOs {
-    public static class RegistrationRequest {
-        private String personalNo;
-        private String email;
-        private String password;
-        private User.UserType userType;
-
-        public String getPersonalNo() { return personalNo; }
-        public void setPersonalNo(String personalNo) { this.personalNo = personalNo; }
-        public String getEmail() { return email; }
-        public void setEmail(String email) { this.email = email; }
-        public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
-        public User.UserType getUserType() { return userType; }
-        public void setUserType(User.UserType userType) { this.userType = userType; }
-    }
-
     public static class LoginRequest {
         private String personalNoOrEmail;
         private String password;
