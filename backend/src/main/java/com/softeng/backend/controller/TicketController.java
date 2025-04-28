5386545c51dafb0bd3406dd61ac68f7c927c6120
Doğukan Baş <bdgknn@gmail.com>
2025-04-28 19:41:13 +0300

    bugfix http request query to body

--- backend/src/main/java/com/softeng/backend/controller/TicketController.java
+++ backend/src/main/java/com/softeng/backend/controller/TicketController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.Base64;
@@ -42,7 +39,7 @@ public class TicketController {
 
     @Transactional
     @PostMapping("/create-ticket")
-    public ResponseEntity<?> createTicket(TicketDTOs.CreateTicketRequest request, Authentication authentication) {
+    public ResponseEntity<?> createTicket(@RequestBody TicketDTOs.CreateTicketRequest request, Authentication authentication) {
         logger.debug("Creating new ticket with cost type: {}", request.getCostType());
         String personalNo = authentication.getName();
         User currentUser = userService.getUserByPersonalNo(personalNo);
