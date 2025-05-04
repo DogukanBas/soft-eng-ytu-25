8421ca851cd47b759adc9fd112a7957ba5fb1ee6
Doğukan Baş <bdgknn@gmail.com>
2025-05-05 01:57:12 +0300

    fix loginfragment addtobackstack issue

--- mobile/app/src/main/java/com/example/mobile/ui/login/LoginFragment.kt
+++ mobile/app/src/main/java/com/example/mobile/ui/login/LoginFragment.kt
@@ -14,6 +14,7 @@ import com.example.mobile.model.User.UserType
 import com.example.mobile.ui.accountant.AccountantMenuFragment
 import com.example.mobile.ui.team_member.TeamMemberMenuFragment
 import com.example.mobile.ui.admin.AdminMenuFragment
+import com.example.mobile.ui.manager.ManagerMenuFragment
 import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -66,18 +67,19 @@ class LoginFragment : BaseFragment() {
                         replaceFragment(AdminMenuFragment(), false)
                     }
                     UserType.MANAGER -> {
+                        replaceFragment(ManagerMenuFragment(),false)
 
 
                     }
                     UserType.TEAM_MEMBER -> {
                         replaceFragment(
-                            TeamMemberMenuFragment()
+                            TeamMemberMenuFragment(),false
                         )
 
                         }
                         UserType.ACCOUNTANT -> {
                             replaceFragment(
-                                AccountantMenuFragment()
+                                AccountantMenuFragment(),false
                             )
 
                     }
